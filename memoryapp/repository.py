322c5246fca8a6c4f792259b61aed0b42e66b48b
wiltos <pawel.wiltos@gmail.com>
2023-07-08 14:36:01 +0200

    Pobieranie listy kart dla kategorii

--- memoryapp/repository.py
+++ memoryapp/repository.py
@@ -1,11 +1,18 @@
 from memoryapp.exceptions import NotFoundException
-from memoryapp.models import Category
+from memoryapp.models import Category, Card
 
 categories_list = [
     Category(1, 'Dom'),
     Category(2, 'Rodzina'),
 ]
+
+cards_list = [
+    Card(1, 1, 'Drzwi', 'Door'),
+    Card(2, 1, 'Okno', 'Window')
+]
+
 id_categories = 2
+id_cards = 2
 
 
 def get_categories():
@@ -35,6 +42,18 @@ def delete_category(category_id):
         categories_list[:] = [category for category in categories_list if category.category_id != category_id]
     else:
         raise NotFoundException('Category')
+
+
+def get_cards(category_id):
+    categories_results = [category for category in categories_list if category.category_id == category_id]
+
+    if not categories_results:
+        raise NotFoundException('Category')
+
+    cards_results = [card for card in cards_list if card.category_id == category_id]
+
+    return cards_results
+
 
 def __next_category_id():
     global id_categories
