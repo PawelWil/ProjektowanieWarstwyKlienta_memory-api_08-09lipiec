8102e96a77368c5524999d67ae6b72ad4325f410
wiltos <pawel.wiltos@gmail.com>
2023-07-08 12:33:20 +0200

    Pobieranie Kategorii

--- memoryapp/repository.py
+++ memoryapp/repository.py
@@ -6,6 +6,7 @@ categories_list = [
 ]
 id_categories = 2
 
+
 def get_categories():
     return categories_list
 
@@ -15,6 +16,13 @@ def create_category(category_name):
     categories_list.append(category)
 
     return category
+
+
+def get_category(category_id):
+    results = [category for category in categories_list if category.category_id == category_id]
+
+    return results[0]
+
 
 def __next_category_id():
     global id_categories
